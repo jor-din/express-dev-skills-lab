a7a8191d38475e3b68cc2bc58974b86673dff194
jordinhuang <jordinhuang@gmail.com>
2022-06-16 18:43:03 -0400

    add edit page

--- controller/skills.js
+++ controller/skills.js
@@ -5,7 +5,8 @@ function index(req, res) {
     Skill.find({})
     .then(skills => {
         res.render('skills/index', {
-            skills:skills
+            skills:skills,
+            time: req.time
         })
     })
     .catch(error=> {
@@ -48,9 +49,49 @@ function show(req, res) {
     })
   }
 
+function deleteSkill(req,res) {
+    Skill.findByIdAndDelete(req.params.id)
+    .then(todo => {
+        res.redirect('/skills')
+    })
+    .catch(error => {
+        console.log(error)
+        res.redirect('/skills')
+      })
+}
+
+function edit(req, res) {
+    Skill.findById(req.params.id)
+    .then(skill => {
+      res.render('skills/edit', {
+        skill: skill
+      })
+    })
+    .catch(error => {
+      console.log(error)
+      res.redirect('/skills')
+    })
+}
+
+function update(req, res) {
+    req.body.start = !!req.body.start
+    console.log(req.body.start)
+    Skill.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(skill => {
+      res.redirect(`/skills/${skill._id}`)
+    })
+    .catch(error => {
+      console.log(error)
+      res.redirect('/skills')
+    })
+  }
+
 export {
     index,
+    show,
     newSkill as new,
     create,
-    show,
+    deleteSkill as delete,
+    edit,
+    update
 }
